5e5ac8fcdbf04da8ed6adf551389f4401e8e57f9
Antonio Zarate Rosas <za447459@uaeh.edu.mx>
2023-05-03 22:13:35 -0600

    Ya agrega un usuario en Kahoot

--- PracticaKahoot/Form1.cs
+++ PracticaKahoot/Form1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Collections.Generic;
 
 //Código del kahoot 271 4095
 
@@ -7,6 +11,10 @@ namespace PracticaKahoot
 {
     public partial class Form1 : Form
     {
+        string codigoJuego;
+        Stack<string> usuarios = new Stack<string>();
+        IWebDriver driver;
+
         public Form1()
         {
             InitializeComponent();
@@ -53,18 +61,54 @@ namespace PracticaKahoot
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
+            guardarDatos();
+            abrirPagina("https://kahoot.it/");
 
+            agregarUsuario(usuarios.Pop());
+
+            //foreach (string usuario in usuarios)
+            //    agregarUsuario(usuario);
         }
 
-        private void agregarUsuarios()
+        private void guardarDatos()
         {
-            //Ingresar a la pagina https://kahoot.com/
-            //Darle clic al boton "Play" con id (menu-item-92364)
-            //Escribir en el único input que hay, el código del juego
+            codigoJuego = txtCodigoJuego.Text;
+            foreach (Control control in Controls)
+                if (control is TextBox && control.Enabled && control.Name != "txtCodigoJuego")
+                    usuarios.Push(control.Text);
+        }
+
+        private void agregarUsuario(string usuario)
+        {
+            //Escribir el código del juego en el único input que hay
+            IWebElement inputCodigo = driver.FindElement(By.Name("gameId"));
+            inputCodigo.SendKeys(codigoJuego);
+            Thread.Sleep(2000);
+
             //Presionar en el único boton que hay
+            IWebElement botonEnter = driver.FindElement(By.TagName("button"));
+            botonEnter.Click();
+            Thread.Sleep(2000);
+
             //Escribir el nombre de usuario en el único input que hay
+            IWebElement inputUsername = driver.FindElement(By.Name("nickname"));
+            inputUsername.SendKeys(usuario);
+            Thread.Sleep(2000);
+
             //Presionar en el único boton que hay
-            //Repetir los últimos dos pasos dependiendo del número de usuarios que haya
+            IWebElement botonOkGo = driver.FindElement(By.TagName("button"));
+            botonOkGo.Click();
+            Thread.Sleep(2000);
+        }
+
+        private void abrirPagina(string url)
+        {
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            driver = new ChromeDriver(chromeDriverService, new ChromeOptions());
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
         }
 
         private void txtCodigoJuego_KeyPress(object sender, KeyPressEventArgs e)
